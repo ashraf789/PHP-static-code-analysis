b3ecca8056937596743da507aea42df8551b58f0
Ashraf789 <syedashrafullah15@gmail.com>
2020-01-03 15:23:42 +0700

    test commit

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class testController extends Controller{
+    private function testTest()
+    {
+    }
+}
---
+++ app/Model/test.php
@@ -0,0 +1,9 @@
+<?php
+namespace App\Model;
+
+class TController
+{
+    private function tt()
+    {
+    }
+}
