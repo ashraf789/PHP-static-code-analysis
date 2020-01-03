8f5f3997385d1eea41bc54cf3b1c859da5df3b9e
Ashraf789 <syedashrafullah15@gmail.com>
2020-01-03 15:24:31 +0700

    test2

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -4,7 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class testController extends Controller{
+class TestController extends Controller
+{
     private function testTest()
     {
     }
